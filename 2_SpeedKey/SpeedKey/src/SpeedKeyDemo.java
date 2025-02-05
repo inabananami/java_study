63c79da290f7a5e6f40f3fe0169b8e9321b4fd5f
新たな旅 <142149583+inabananami@users.noreply.github.com>
2025-02-05 16:56:39 +0800

    day1 finished
    -

---
+++ 2_SpeedKey/SpeedKey/src/SpeedKeyDemo.java
@@ -0,0 +1,7 @@
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        //これは一行のコメントです
+        /*　これは複数行のコメントです
+           Javaの学習に行くよ！*/
+    }
+}
---
+++ 3_基本的な変数/VariableDemo/src/VariableDemo.java
@@ -0,0 +1,28 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        int a = 0;
+        float b = 1.11f;
+        double c = 1.11;
+        char d = 'こ';
+        String e = "こんにちは";
+        boolean f = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        a = 1;
+        b = 2.22f;
+        c = 2.22;
+        d = 'に';
+        e = "こんばんは";
+        f = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
